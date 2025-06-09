e7f7b7ac9de7767ffb34d57248b09d507a26a025
Rut Arisleidy Santos <arisleidy@MacBook-Pro-de-Rut.local>
2025-06-08 21:26:19 -0400

    Actualización completa: se corrigieron exportaciones, se añadieron secciones animadas (Favoritos, Carrito) y se optimizó el diseño general

---
+++ src/pages/Nosotros.jsx
@@ -0,0 +1,10 @@
+function Nosotros() {
+    return (
+    <div className="p-6">
+        <h1 className="text-3xl font-bold">Nosotros</h1>
+        <p>Bienvenido a la sección de Nosotros. Aquí hablamos sobre quiénes somos.</p>
+    </div>
+    );
+}
+
+export default Nosotros;
