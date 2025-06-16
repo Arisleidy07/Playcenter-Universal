1fe5e2e27c0dde7c0f648cf9bce15fe67b68cb46
Rut Arisleidy Santos <arisleidy@MacBook-Pro-de-Rut.local>
2025-06-16 12:18:49 -0400

    Eliminado título 'Todos los productos' en Productos.jsx

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,27 +1,14 @@
 import React from "react";
-import "../styles/blobCard.css"; // Tus estilos para las tarjetas
-import { Outlet, useLocation } from "react-router-dom";
+import "../styles/blobCard.css";
+import { Outlet } from "react-router-dom";
 import SidebarCategorias from "../components/SidebarCategorias";
 
 function Productos() {
-  const location = useLocation();
-  // Verifica si estás en la ruta exacta "/productos"
-  const esVistaGeneral = location.pathname === "/productos";
-
   return (
-    <div className="flex pt-20 min-h-screen bg-white">
-      {/* Barra lateral con las categorías */}
+    <div className="flex flex-col sm:flex-row pt-20 min-h-screen bg-white">
       <SidebarCategorias />
 
-      {/* Contenido principal, área donde se renderizan los productos o subcategorías */}
-      <main className="flex-1 p-4">
-        {/* Título solo si estás en la vista general de productos */}
-        {esVistaGeneral && (
-          <h1 className="text-2xl font-bold mb-6 text-gray-800">
-            Todos los productos
-          </h1>
-        )}
-        {/* Aquí se renderiza el contenido anidado, ej: ProductosTodos o categoría */}
+      <main className="flex-1 p-2 sm:p-4">
         <Outlet />
       </main>
     </div>
