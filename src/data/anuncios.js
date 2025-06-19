20c3e1daedc4ec9bf1539d7a5a05be6e15511a34
Rut Arisleidy Santos <arisleidy@MacBook-Pro-de-Rut.local>
2025-06-19 17:33:23 -0400

    🔥 Arreglados los enlaces del slider de anuncios

--- src/data/anuncios.js
+++ src/data/anuncios.js
@@ -1,32 +1,7 @@
 const anuncios = [
-  {
-    id: 1,
-    img: "/ads/consolas.png",
-    link: "/productos/consolas",
-    isExternal: false,
-  },
-  {
-    id: 2,
-    img: "/ads/ubicacion.png",
-    link: "https://maps.app.goo.gl/ZSXza3ESVeLEPPx78",
-    isExternal: true,
-  },
-  {
-    id: 3,
-    img: "/ads/videojuegos.png",
-    link: "/productos/videojuegos",
-    isExternal: false,
-  },
-  {
-    id: 4,
-    img: "/ads/verofertas.png",
-    link: "/productos/ofertas-especiales",
-    isExternal: false,
-  },
-  {
-    id: 5,
-    img: "/ads/productos.png",
-    link: "/productos",
-    isExternal: false,
-  },
+  { id: 1, img: "/ads/consolas.png", link: "/productos/consolas", isExternal: false },
+  { id: 2, img: "/ads/ubicacion.png", link: "https://maps.app.goo.gl/ZSXza3ESVeLEPPx78", isExternal: true },
+  { id: 3, img: "/ads/videojuegos.png", link: "/productos/videojuegos", isExternal: false },
+  { id: 4, img: "/ads/verofertas.png", link: "/productos/ofertas-especiales", isExternal: false },
+  { id: 5, img: "/ads/productos.png", link: "/productos", isExternal: false },
 ];
