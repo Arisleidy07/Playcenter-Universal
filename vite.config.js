d297bcc6ea1164ab30772ed032413430bbc1ac4a
Rut Arisleidy Santos <arisleidy@MacBook-Pro-de-Rut.local>
2025-07-07 23:22:49 -0400

    Actualización de estilo y estructura en la página principal

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: './',
+  base: "./",
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Alias para Firebase para evitar problemas con módulos internos
-      'firebase/app': 'firebase/compat/app',
-      'firebase/auth': 'firebase/compat/auth',
-      'firebase/firestore': 'firebase/compat/firestore',
-      'firebase/storage': 'firebase/compat/storage',
-      // Agrega más según lo que uses de Firebase
-    },
+  optimizeDeps: {
+    include: ["firebase/app", "firebase/auth", "firebase/analytics"],
   },
-})
+});
